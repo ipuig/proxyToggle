6f7d467d1d9d8287e5849e470078b451f9aa07d6
Ivan puig <ivan@bpuig.com>
2024-05-31 15:12:01 +0100

    Notify the user when insufficient permissions

--- popup/popup.js
+++ popup/popup.js
@@ -53,7 +53,18 @@ const update = () => {
 
 
 window.onload = () => {
-    loadProxySettings();
-    document.getElementById("submit").onclick = toggle;
-    document.getElementById("update").onclick = update;
+    browser.extension.isAllowedIncognitoAccess()
+        .then(isAllowed => {
+
+            if (!isAllowed) {
+                const page = document.getElementsByTagName("body")[0];
+                page.innerHTML = '<h1 align="center">Not Enough Permissions</h1><h2 align="center">To modify proxy settings you must allow access on private windows</h2><h3 style="width: 60%; margin: 0 auto; margin-bottom: .5em;">Follow this steps</h3><ol style="width: 60%; margin: 0 auto;"><li>Go to <b>about:addons</b> from your search bar</li><li>Select the proxy toggle extension</li><li>Go to details</li><li>Allow run in Private Windows</li><li>Next time you open the popup everything should work as intended</li></ol><p align="center"><img src="../assets/img/incognito.png" width="60%"></p>'
+                page.style.padding = "1em";
+                return;
+            }
+
+            loadProxySettings();
+            document.getElementById("submit").onclick = toggle;
+            document.getElementById("update").onclick = update;
+        })
 }
